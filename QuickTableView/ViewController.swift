0a54bed14970c4f9576f00e337640a5e8d2e09d2
Ella Madalinski <ella.madalinski24@d155.org>
2021-10-18 08:11:09 -0500

    Initial Commit

---
+++ QuickTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuickTableView
+//
+//  Created by ELLA MADALINSKI on 10/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
